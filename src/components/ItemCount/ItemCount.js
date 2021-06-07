cf2eef9cffd643ea26e7d70a6b2b768c6119d290
Gianfranco Mastogiovanni <gianmasto1@gmail.com>
2021-06-06 21:19:13 -0300

    ItemCount component added

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+
+import './ItemCount.css'
+
+
+function ItemCount({stock, inital, onAdd}) {
+
+  const [itemsCounter, setItemsCounter] = useState(inital || 0)
+
+  const onItemsCounterSubstract = () => {
+    if(itemsCounter > 0) {
+      setItemsCounter(itemsCounter - 1)
+    } 
+  }
+
+  const onItemsCounterAdd = () => {
+    if(itemsCounter < stock) {
+      setItemsCounter(itemsCounter + 1)
+    } 
+  }
+
+  return (
+    <div className="item-count-container">
+      <div className="controls">
+        <span>
+          <button className={itemsCounter <= 0 ? 'disabled' : null} onClick={onItemsCounterSubstract}>-</button>
+        </span>
+        <span className="counter-number">
+          <p>{itemsCounter}</p>
+        </span>
+        <span>
+          <button className={itemsCounter >= stock ? 'disabled' : null} onClick={onItemsCounterAdd}>+</button>
+        </span>
+      </div>
+      <button className={`add-button ${itemsCounter <= 0 ? ' disabled' : ''}`} onClick={() => onAdd(itemsCounter)}>Agregar al carrito</button>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,18 @@
 import './ItemListContainer.css';
+import ItemCount from '../../components/ItemCount/ItemCount'
 
 function ItemListContainer({greeting}) {
+
+  const onAdd = (count) => {
+    if(count > 0) {
+      console.log(`${count} productos comprados`);
+    } 
+  }
+
   return (
     <div className="container">
       <h2>{greeting}</h2>
+      <ItemCount stock={10} inital={0} onAdd={onAdd} />
     </div>
   );
 }
